85013f709f3356efc888c015e54a10bcfd025a27
Adriana Paluszny <apaluszn@imperial.ac.uk>
2020-01-17 20:55:04 +0000

    Add files via upload

--- main_acse5_lecture1.cpp
+++ main_acse5_lecture1.cpp
@@ -5,36 +5,36 @@
 
 using namespace std;
 
+//n!
+template<typename T>
+T fact(T number)
+{
+	T factorial = 1;
+	for (T i = 1; i <= number; i++)
+	{
+		factorial *= i;
+	}
+	return factorial;
+}
+
 int main()
 {
-	//streams
-	cout << "\nHello world!\n"; //standard output
-	cerr << "\nHello world ERROR!\n"; //standard error
+	//look at the differences in the output when we change the types
+	//what is happening?
+	/*cerr << "\nFactorials ";
+	for (double i = 0; i < 100; i++)//try 500
+	{
+		//cerr << "\n" << i << ":\t" << fact(i);
+		cerr << "\n" << i << ":\t" << fact<int>(i);
+		cerr << ":" << fact<unsigned int>(i);
+		cerr << ":" << fact<long long>(i);
+		cerr << ":" << fact<float>(i);
+		cerr << ":" << fact<double>(i);
+		cerr << ":" << fact<long double>(i);
+	}
+	cin.get();*/
 
-	cout << "\nHey! what is your name and age? ...";
-
-	int iAge(0);// , j(1), k(2);
-	//k = i + j;
-
-	string name, last_name;
-	cin >> name >> last_name >> iAge;
-	string full_name(name + " " + last_name);
-	
-	cout << "\n\nHey! Your name is " << name << " " << last_name;
-	cout << "\n\nHey! Your FULL name is " << full_name << " and age " << iAge; 
-
-	//streams
-	ofstream ofs("output_file.txt", ios::trunc);
-	ofs << "\n\nHey! Your name is " << name << " " << last_name;
-	ofs.close();
-
-	int a = -1;
-	unsigned int ua = 2;
-	double f = 2.3;
-	float ff = 1.2;
-
-	system("pause");
-
+	return 0;
 	return 0;
 };
 
