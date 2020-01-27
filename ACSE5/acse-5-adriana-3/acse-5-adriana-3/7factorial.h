1829567d38f7ddad1598294c56f707bcc5cf0685
Hao Lu <hl1319@ic.ac.uk>
2020-01-27 09:07:23 +0000

    first commit

---
+++ ACSE5/acse-5-adriana-3/acse-5-adriana-3/7factorial.h
@@ -0,0 +1,13 @@
+//
+//  7factorial.h
+//  acse-5-adriana-3
+//
+//  Created by 路昊 on 16/01/2020.
+//  Copyright © 2020 luhao. All rights reserved.
+//
+
+#ifndef _factorial_h
+#define _factorial_h
+
+
+#endif /* _factorial_h */
---
+++ ACSE5/acse-5-lecture-2/acse-5-lecture-2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  acse-5-lecture-2
+//
+//  Created by 路昊 on 14/01/2020.
+//  Copyright © 2020 luhao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
